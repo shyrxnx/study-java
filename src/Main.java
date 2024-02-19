9bfc42d32811f62b235031d955e6f92ea7ca38a5
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-02-19 14:07:45 +0800

    feat: Add Java program to calculate sum, difference, and product of two numbers

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number_one, number_two, sum, difference, product;
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("This program calculates the sum, difference, and product of two inputs!\n");
+        System.out.print("Enter the first number: ");
+        number_one = inputScanner.nextInt();
+        System.out.print("Enter the second number: ");
+        number_two = inputScanner.nextInt();
+
+        sum = number_one + number_two;
+        difference = number_one - number_two;
+        product = number_one * number_two;
+
+        System.out.println("\nThe results of: " + number_one + " and " + number_two);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+    }
+}
