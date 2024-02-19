07a156c1192473a61ac01087fe1dd1ef168df367
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-02-19 14:24:08 +0800

    feat: add A Java program for practice on Java's data types

---
+++ src/VariablesPractice.java
@@ -0,0 +1,28 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        // Integer Data Types
+        byte byteVariable = 10;
+        short shortVariable = 1000;
+        int intVariable = 100000;
+        long longVariable = 1000000000L;
+
+        // Floating-point Data Types
+        float floatVariable = 3.14f;
+        double doubleVariable = 3.14159265359;
+
+        // Character Data Type
+        char charVariable = 'A';
+
+        // Boolean Data Type
+        boolean booleanVariable = false;
+
+        System.out.println("Byte Variable: " + byteVariable);
+        System.out.println("Short Variable: " + shortVariable);
+        System.out.println("Int Variable: " + intVariable);
+        System.out.println("Long Variable: " + longVariable);
+        System.out.println("Float Variable: " + floatVariable);
+        System.out.println("Double Variable: " + doubleVariable);
+        System.out.println("Char Variable: " + charVariable);
+        System.out.println("Boolean Variable: " + booleanVariable);
+    }
+}
