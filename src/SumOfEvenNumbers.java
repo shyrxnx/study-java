cd994f004741531f475ede86d265278bf1ccc530
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-02-20 16:33:18 +0800

    refactor: optimize calculation of sum of even numbers

--- src/SumOfEvenNumbers.java
+++ src/SumOfEvenNumbers.java
@@ -4,15 +4,13 @@ public class SumOfEvenNumbers {
     public static void main(String[] args) {
         Scanner getUserInput = new Scanner(System.in);
 
-        System.out.print("Enter an even number: ");
+        System.out.print("Enter a positive integer: ");
         int number = getUserInput.nextInt();
 
         int sum = 0;
 
-        for (int i = 1; i <= number; i++) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
+        for (int i = 2; i <= number; i += 2) {
+            sum += i;
         }
         System.out.println("The sum of all even numbers between 1 to " + number + " is: " + sum);
     }
