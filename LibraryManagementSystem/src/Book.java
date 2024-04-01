7d95fbbd140b0792172c07889487dc3c36cb1b88
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-04-01 18:24:52 +0800

    feat: Provided setters and getters for attributes

--- LibraryManagementSystem/src/Book.java
+++ LibraryManagementSystem/src/Book.java
@@ -1,2 +1,49 @@
 public class Book {
+
+    private String title;
+    private String author;
+    private String ISBN;
+    private boolean isAvailable;
+
+    Book(String title, String author, String ISBN, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getTitle(String title) {
+        return title;
+    }
+
+    public String getAuthor(String author) {
+        return author;
+    }
+
+    public String ISBN(String ISBN) {
+        return ISBN;
+    }
+
+    public boolean isAvailable(boolean isAvailable) {
+        return isAvailable;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+
+
 }
--- LibraryManagementSystem/src/Member.java
+++ LibraryManagementSystem/src/Member.java
@@ -1,2 +1,36 @@
 public class Member {
+
+    private String name;
+    private String email;
+    private String memberID;
+
+    Member(String name, String email, String memberID) {
+        this.name = name;
+        this.email = email;
+        this.memberID = memberID;
+    }
+
+    public String getName(String name) {
+        return name;
+    }
+
+    public String getEmail(String email) {
+        return email;
+    }
+
+    public String getMemberID(String memberID) {
+        return memberID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
 }
